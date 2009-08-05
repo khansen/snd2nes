8b65c8185ddbd39e3de9fc6962ff6c6c9eec1d86
Kent Hansen <khansen@trolltech.com>
2009-08-05 20:13:57 +0200

    add license header

--- snd2nes.c
+++ snd2nes.c
@@ -1,3 +1,20 @@
+/*
+    This file is part of snd2nes.
+
+    snd2nes is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snd2nes is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snd2nes.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <math.h>
 #include <sndfile.h>
 #include <stdio.h>
